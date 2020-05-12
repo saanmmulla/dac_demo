057a0e64a45eef977165c78bf69cb0e7670c9f20
saanmulla <sanammulla3@gmail.com>
2020-05-11 18:19:13 -0700

    This is my first code

---
+++ MissFirst.cpp
@@ -0,0 +1,26 @@
+//Missing number
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int bi[10];
+	int index=0,r=0;
+	
+	for(int i=0;n!=0;i++)
+	{
+		r=n%2;
+		n=n/2;
+		bi[index]=r;
+		index++;
+	}
+	
+	for(int i=index-1;i>=0;i--)
+	{
+		cout<<bi[i];
+	}
+return 0;
+}
